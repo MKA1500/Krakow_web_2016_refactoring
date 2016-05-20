ad6e309de422ed9cbfa8e606afcb3a5cc6bd1263
MKA1500 <maciejkabsch@gmail.com>
2016-05-20 21:56:18 +0200

    ...

--- krakow.js
+++ krakow.js
@@ -2,30 +2,105 @@
  $(document).ready(function() {
   
     $('a').hover(function(){
-	  $(this).toggleClass("active");
+	  $(this).toggleClass("teraz");
 	}); 	
 	
- 	 var view = 0; // 0 == main/visit; 1 == culture; 2 == study; 3 == night life;
+ 	 var view = 0; 
  
-	$("#big-two").css("opacity", 0 );		 
-	$("#big-three").css("opacity", 0 ); 
-    $("#big-four").css("opacity", 0 );
+	$("#big-two").css("opacity", 0 ).css("display", "none");		 
+	$("#big-three").css("opacity", 0 ).css("display", "none"); 
+    $("#big-four").css("opacity", 0 ).css("display", "none");
     
-    // $("#visit").css("color", "#FFFF00");	----- the hover' don't work yet after changing the color of the link                                    // I'll try to fix it, the work to be continued...      
+     $("#visit").css("color", "#FFFF00");    
 	
     $("#culture").bind('click', function () {
 		if(view == 0) {
 		   $("#big-one").fadeTo( "slow", 0 ).css("display", "none");		
 		   $("#big-two").css("display", "block").fadeTo( "slow", 1 );	
-		}
-     /*      $("#visit").css("color", "#FFFFFF");
-		  $("#culture").css("color", "#FFFF00");
+           $("#visit").css("color", "#FFFFFF");
+		   $("#culture").css("color", "#FFFF00");
 		   
 		} else if (view == 2) {
-		   $("").fadeTo( "slow", 0 ).css("display", "none");		
-		   $("").css("display", "block").fadeTo( "slow", 1 );			   
-		}
-	view = 1;	*/ 
+		   $("#big-three").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-two").css("display", "block").fadeTo( "slow", 1 );	
+           $("#study").css("color", "#FFFFFF");
+		   $("#culture").css("color", "#FFFF00");	
+		   
+		} else if (view == 3) {
+		   $("#big-three").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-two").css("display", "block").fadeTo( "slow", 1 );	
+           $("#night-life").css("color", "#FFFFFF");
+		   $("#culture").css("color", "#FFFF00");		   
+		} 
+	view = 1;	 
+	});	
+
+    $("#study").bind('click', function () {
+		if(view == 0) {
+		   $("#big-one").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-three").css("display", "block").fadeTo( "slow", 1 );	
+           $("#visit").css("color", "#FFFFFF");
+		   $("#study").css("color", "#FFFF00");
+		   
+		} else if (view == 1) {
+		   $("#big-two").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-three").css("display", "block").fadeTo( "slow", 1 );	
+           $("#culture").css("color", "#FFFFFF");
+		   $("#study").css("color", "#FFFF00");	
+		   
+		} else if (view == 3) {
+		   $("#big-four").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-three").css("display", "block").fadeTo( "slow", 1 );	
+           $("#night-life").css("color", "#FFFFFF");
+		   $("#study").css("color", "#FFFF00");		   
+		} 
+	view = 2;	 
 	});	
 	
+    $("#night-life").bind('click', function () {
+		if(view == 0) {
+		   $("#big-one").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-four").css("display", "block").fadeTo( "slow", 1 );	
+           $("#visit").css("color", "#FFFFFF");
+		   $("#night-life").css("color", "#FFFF00");
+		   
+		} else if (view == 1) {
+		   $("#big-two").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-four").css("display", "block").fadeTo( "slow", 1 );	
+           $("#culture").css("color", "#FFFFFF");
+		   $("#night-life").css("color", "#FFFF00");	
+		   
+		} else if (view == 2) {
+		   $("#big-three").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-four").css("display", "block").fadeTo( "slow", 1 );	
+           $("#study").css("color", "#FFFFFF");
+		   $("#night-life").css("color", "#FFFF00");		   
+		} 
+	view = 3;	 
+	});	
+	
+	// 0 == main/visit, big-one; 
+	// 1 == culture, big-two; 
+	// 2 == study, big-three; 
+	// 3 == night life, big-four;
+    $("#visit").bind('click', function () {
+		if(view == 1) {
+		   $("#big-two").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-one").css("display", "block").fadeTo( "slow", 1 );	
+           $("#culture").css("color", "#FFFFFF");
+		   $("#visit").css("color", "#FFFF00");
+		   
+		} else if (view == 2) {
+		   $("#big-three").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-one").css("display", "block").fadeTo( "slow", 1 );	
+           $("#study").css("color", "#FFFFFF");
+		   $("#visit").css("color", "#FFFF00");		   
+		} else if (view == 3) {
+		   $("#big-four").fadeTo( "slow", 0 ).css("display", "none");		
+		   $("#big-one").css("display", "block").fadeTo( "slow", 1 );	
+           $("#night-life").css("color", "#FFFFFF");
+		   $("#visit").css("color", "#FFFF00");		   
+		} 
+	view = 0;	 
+	});	
 });
